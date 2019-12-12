745f48b25ad1fcfe289f6d6e6ae1808f0f1a3dce
shiwei.yu <maverick923@163.com>
2019-12-12 10:52:39 +0800

    update access token

--- src/main/java/com/dshc/chuhai/test/TestFBJavaSDK.java
+++ src/main/java/com/dshc/chuhai/test/TestFBJavaSDK.java
@@ -5,16 +5,29 @@ import com.facebook.ads.sdk.APINodeList;
 import com.facebook.ads.sdk.AdAccount;
 import com.facebook.ads.sdk.Campaign;
 
-public class TestFBJavaSDK
-{
+import java.util.Arrays;
+
+public class TestFBJavaSDK {
+    private static String accesstoken = "EAAKpzJnM2LoBANOxZAAPtISTbZCvc8IoZADplccT3y5VZAZCa0dbPO3pF582vivYuvtMB8NMcrAVIgOKCYAGlIFv0PHDRxYzM1CKiKr5vnJJYrgKEtBeT4PCc5kFpBCJjyo5G3xDmDmF3bDRUbRdtJsVWQKYT8fZCb3CFbJkdI4Et8vU9iV3KZBJ7IWT2bli8ZAbxGuyUt4U66pYZAxVT2Y1g";
+
+    private static String appsecret = "ccc29740c3b7767b7d17741f60dc4ade";
+
+
     public static final APIContext context = new APIContext(
-            "{access-token}",
-            "{appsecret}"
-    );
+            accesstoken,
+            appsecret
+    ).enableDebug(true);
+
+    //public static final APIContext context = new APIContext(accesstoken).enableDebug(true);
+
     public static void main(String[] args)
     {
-        AdAccount account = new AdAccount("act_{{adaccount-id}}", context);
+        AdAccount account = new AdAccount("2474347719357584", context);
         try {
+            APINodeList<Campaign> camps = account.getCampaigns().requestAllFields().execute();
+            if( camps == null ){
+                System.out.println("APIRequestGetCampaigns is null");
+            }
             APINodeList<Campaign> campaigns = account.getCampaigns().requestAllFields().execute();
             for(Campaign campaign : campaigns) {
                 System.out.println(campaign.getFieldName());
@@ -22,4 +35,9 @@ public class TestFBJavaSDK
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }}
+    }
+}
+
+
+
+
